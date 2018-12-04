5d76d4a251339fd83ff86693f09ec04c14a1661e
YuXiang <richardlin0212@gmail.com>
2018-12-05 01:17:32 +0800

    Upload to imgur.com

--- build_image.py
+++ build_image.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import pyimgur
 
 def bind_image(imagelist, sender):
     rstPic = Image.new('RGBA', (180,180), (0,0,0,0))
@@ -14,6 +15,18 @@ def bind_image(imagelist, sender):
     img_addr = "binded_image/"+sender+".png"
     #儲存新的照片
     rstPic.save(img_addr)
+    
+    CLIENT_ID = "9150a06aa5d4a85"
+    PATH = img_addr
 
-    return img_addr
+    im = pyimgur.Imgur(CLIENT_ID)
+    uploaded_image = im.upload_image(PATH, title=sender)
+    #print(uploaded_image.title)
+    #print(uploaded_image.link)
+    #print(uploaded_image.size)
+    #print(uploaded_image.type)
+    
+    
+
+    return uploaded_image.link
 
