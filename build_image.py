e9df45a90a5e2b4cb7402fc7a83aa7db58fbc35f
YuXiang <richardlin0212@gmail.com>
2018-12-06 23:24:26 +0800

    Support protobuf

--- build_image.py
+++ build_image.py
@@ -1,7 +1,55 @@
 from PIL import Image
 import pyimgur
+import image_pb2
+import sys
+
+my_pb_file = "image.pb"
+
+my_all_image = image_pb2.all_image()
+
+with open(my_pb_file, "rb") as f:
+    my_all_image.ParseFromString(f.read())
+
+def rebuild_list(imagelist):
+    graph = [["_","_","_"],
+            ["_","_","_"],
+            ["_","_","_"]]
+    for i in range(0, len(imagelist)):
+        j = -1
+        if imagelist[i][0] == "A":
+            j = 0
+        elif imagelist[i][0] == "B":
+            j = 1
+        elif imagelist[i][0] == "C":
+            j = 2
+
+        k = -1
+        if imagelist[i][1] == "1":
+            k = 0
+        elif imagelist[i][1] == "2":
+            k = 1
+        elif imagelist[i][1] == "3":
+            k = 2
+
+        if i%2 == 0:
+            graph[k][j] = "o"
+        else:
+            graph[k][j] = "x"
+
+    ooxx_str = ""
+    for i in range(0, 9):
+        ooxx_str = ooxx_str + graph[int(i/3)][i%3]
+
+    return ooxx_str
 
 def bind_image(imagelist, sender):
+    image_name = rebuild_list(imagelist)
+
+    for image in my_all_image.image:
+        if image.name == image_name:
+            return image.url
+        
+    
     rstPic = Image.new('RGBA', (180,180), (0,0,0,0))
     #open image
     for i in range(0, len(imagelist)):
@@ -26,7 +74,13 @@ def bind_image(imagelist, sender):
     #print(uploaded_image.size)
     #print(uploaded_image.type)
     
-    
+    new_image = my_all_image.image.add()
+
+    new_image.url = uploaded_image.link
+    new_image.name = image_name
+
+    with open(my_pb_file, "ab") as f:
+        f.write(my_all_image.SerializeToString())
 
     return uploaded_image.link
 
---
+++ image.pb
@@ -0,0 +1,3 @@
+
+,
+	o________https://i.imgur.com/vs8pO8r.png
