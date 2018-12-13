410e907194212964679b5261faf6310c91d30a2a
YuXiang <richardlin0212@gmail.com>
2018-12-13 10:44:38 +0800

    Remove .heroku

--- .heroku/python/lib/graphviz/tcl/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-package ifneeded Tcldot 2.38.0 "
-	load [file join $dir libtcldot.0.dylib] Tcldot"
-package ifneeded Tclpathplan 2.38.0 "
-	load [file join $dir libtclplan.0.dylib] Tclpathplan"
-package ifneeded Tkspline 2.38.0 "
-	package require Tk 8.3
-	load [file join $dir libtkspline.0.dylib] Tkspline"
-# end
